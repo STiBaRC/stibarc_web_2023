80e25207544cda083ec746f5ccfbfa1c3932b863
Fennec Border <me@flolon.cc>
2023-10-27 09:22:52 -0400

    add basic profile banner on profile.html

--- js/user.js
+++ js/user.js
@@ -30,6 +30,7 @@ window.addEventListener("load", async () => {
 	user = requestJSON.user;
 	if (user.verified) $("#userverified").classList.remove("hidden");
 	$("#userpfp").setAttribute("src", user.pfp);
+	$("#userBanner").src = user.banner;
 	$("#name").innerText = `Real name: ${user.name}`;
 	if (user.pronouns) $("#pronouns").innerText = `(${user.pronouns})`;
 	$("#email").innerText = `Email: ${user.email}`;
