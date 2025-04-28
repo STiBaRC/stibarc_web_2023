122d3d1292da7b3045a06b5d9766b46b8c9c0f2f
Joshua Herron <herronjo@stibarc.dev>
2025-04-27 21:44:16 -0400

    Add developer portal

---
+++ developer/DeveloperAppComponent.js
@@ -0,0 +1,75 @@
+class DeveloperAppComponent extends HTMLElement {
+	// This is safe because no part of this is dynamic
+	#shadowDomHTML = `
+		<style>
+			@import url("/css/global.css");
+
+			:host {
+				display: block;
+				width: 280px;
+				height: 200px;
+			}
+
+			.block {
+				width: 100%;
+			}
+
+			.app-icon {
+				width: 110px;
+				height: auto;
+				border-radius: 10px;
+				margin-bottom: 5px;
+				background-color: var(--color1);
+			}
+
+			#app {
+				width: 280px;
+				height: 200px;
+			}
+		</style>
+		<a class="block flexcontainer flexcolumn pointer" id="app">
+			<img id="icon">
+			<span class="flexcontainer">
+				<span id="name"></span>
+				<stibarc-icon id="verified" type="verifiedBadge" name="verified" class="verifiedBadge hidden" title="Verified"></stibarc-icon>
+			</span>
+		</a>
+	`;
+
+	constructor() {
+		super();
+	}
+
+	connectedCallback() {
+		this.shadow = this.attachShadow({ mode: "closed" });
+		this.shadow.innerHTML = this.#shadowDomHTML;
+
+		const name = this.getAttribute("name") || "Unknown";
+		const id = this.getAttribute("id") || "Unknown";
+		const description = this.getAttribute("description") || "No description available.";
+		const icon = this.getAttribute("icon") || "/images/default-icon.png";
+		const verified = this.getAttribute("verified") === "true" ? true : false;
+		const firstparty = this.getAttribute("firstparty") === "true" ? true : false;
+
+		const iconElement = this.shadow.getElementById("icon");
+		iconElement.src = icon;
+		iconElement.alt = `${name} icon`;
+		iconElement.classList.add("app-icon");
+
+		const nameElement = this.shadow.getElementById("name");
+		nameElement.textContent = name;
+
+		const verifiedElement = this.shadow.getElementById("verified");
+		if (verified) {
+			verifiedElement.classList.remove("hidden");
+		}
+		if (firstparty) {
+			verifiedElement.classList.remove("hidden");
+			verifiedElement.setAttribute("title", "First Party App");
+		}
+
+		this.shadow.getElementById("app").setAttribute("href", `/developer/app.html?id=${id}`);
+	}
+}
+
+customElements.define("stibarc-developer-app", DeveloperAppComponent);
---
+++ developer/app.js
@@ -0,0 +1,226 @@
+let selectedTab = location.hash.slice(1) || "general";
+let isMobile = false;
+
+async function getApp(id) {
+	const response = await fetch(`${api.host}/v4/developer/getappdetails.sjs?client_id=${encodeURIComponent(id)}`, {
+		method: "post",
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify({
+			session: api.session
+		})
+	});
+	const data = await response.json();
+	return data.application;
+}
+
+function switchTab(tabEl) {
+	$(".sidebarItems li").forEach(item => {
+		item.classList.remove("active");
+	});
+	tabEl.classList.add("active");
+	let tab = tabEl.id.replace("tab-", "");
+	location.hash = tab;
+	$(".tabContent").forEach(item => {
+		item.classList.add("hidden");
+	});
+	if (isMobile) {
+		$("#backBtn").classList.remove("hidden");
+		$("#sidebarTabs").classList.add("hidden");
+		if (tab) {
+			$("#sideContent").classList.remove("hidden");
+		}
+	}
+	if (tab || !isMobile) {
+		$(`#tabContent-${tab}`).classList.remove("hidden");
+	}
+}
+
+const mediaQuery = window.matchMedia("(max-width: 475px)");
+
+function handleViewportChange(e) {
+	if (e.matches) {
+		isMobile = true;
+		if (location.hash) {
+			$("#backBtn").classList.remove("hidden");
+			$("#sideContent").classList.remove("hidden");
+			$("#sidebarTabs").classList.add("hidden");
+		} else {
+			$("#backBtn").classList.add("hidden");
+			$("#sideContent").classList.add("hidden");
+			$("#sidebarTabs").classList.remove("hidden");
+		}
+	} else {
+		isMobile = false;
+		$("#backBtn").classList.add("hidden");
+		$("#sideContent").classList.remove("hidden");
+		$("#sidebarTabs").classList.remove("hidden");
+	}
+}
+
+function reloadCallbackUrls(appDetails) {
+	$("#callbackurls").replaceChildren();
+	const entries = document.createDocumentFragment();
+	appDetails.callbackurls.forEach((url, index) => {
+		const container = document.createElement("div");
+		const input = document.createElement("input");
+
+		input.type = "text";
+		input.value = url;
+		input.classList.add("callbackurl");
+		input.placeholder = `Callback URL ${index + 1}`;
+		input.id = `callbackurl-${index}`;
+		input.addEventListener("input", () => {
+			appDetails.callbackurls[index] = input.value;
+		});
+
+		const removeButton = document.createElement("button");
+		removeButton.classList.add("button", "smallBtn");
+		removeButton.setAttribute("title", "Remove callback URL");
+		removeButton.textContent = "X";
+
+		removeButton.addEventListener("click", (e) => {
+			e.preventDefault();
+			appDetails.callbackurls.splice(index, 1);
+			reloadCallbackUrls(appDetails);
+		});
+
+		container.appendChild(input);
+		container.appendChild(removeButton);
+		entries.appendChild(container);
+	});
+	$("#callbackurls").appendChild(entries);
+}
+
+window.addEventListener("load", async () => {
+	$(".sidebarItems li").forEach(item => {
+		item.addEventListener("click", () => {
+			switchTab(item);
+		});
+	});
+	$(".tabContent").forEach(element => {
+		element.classList.add("hidden");
+	});
+	$(`#tab-${selectedTab}`).classList.add("active");
+	if (location.hash || !isMobile) {
+		$(`#tabContent-${selectedTab}`).classList.remove("hidden");
+	}
+
+	if (!api.loggedIn) {
+		window.location.href = `/developer/`;
+	}
+
+	const client_id = new URLSearchParams(window.location.search).get("id");
+	const appDetails = await getApp(client_id);
+
+	if (!appDetails) {
+		window.location.href = `/developer/`;
+		return;
+	}
+
+	document.title = `${appDetails.name} | Developer | STiBaRC`;
+	$("#appnameheader").textContent = appDetails.name;
+	$("#appicon").src = appDetails.icon;
+	$("#appname").value = appDetails.name;
+	$("#appiconurl").value = appDetails.icon;
+	$("#appdesc").textContent = appDetails.description;
+	$("#clientid").textContent = appDetails.id;
+	$("#clientsecret").textContent = appDetails.secret;
+	$("#confirmdeleteinput").setAttribute("placeholder", appDetails.name);
+
+	$("#appname").addEventListener("input", () => {
+		appDetails.name = $("#appname").value;
+		document.title = `${appDetails.name} | Developer | STiBaRC`;
+		$("#appnameheader").textContent = appDetails.name;
+		$("#confirmdeleteinput").setAttribute("placeholder", appDetails.name);
+	});
+	$("#appiconurl").addEventListener("input", () => {
+		appDetails.icon = $("#appiconurl").value;
+		$("#appicon").src = appDetails.icon;
+	});
+	$("#appdesc").addEventListener("input", () => {
+		appDetails.description = $("#appdesc").value;
+	});
+
+	reloadCallbackUrls(appDetails);
+
+	$("#addcallbackurl").addEventListener("click", () => {
+		appDetails.callbackurls.push("");
+		reloadCallbackUrls(appDetails);
+	});
+
+	$("#sideContentLoading").classList.add("hidden");
+
+	handleViewportChange(mediaQuery);
+	mediaQuery.addListener(handleViewportChange);
+
+	$("#backBtn").addEventListener("click", () => {
+		$("#backBtn").classList.add("hidden");
+		$("#sideContent").classList.add("hidden");
+		$("#sidebarTabs").classList.remove("hidden");
+		selectedTab = "";
+		$(".sidebarItems li").forEach(item => {
+			item.classList.remove("active");
+		});
+	});
+
+	$("#saveapp").addEventListener("click", async () => {
+		$("#saveapp").setAttribute("disabled", "true");
+		$("#saveapp").textContent = "";
+		$("#saveapp").classList.add("loading", "small");
+		await fetch(`${api.host}/v4/developer/updateapp.sjs?client_id=${encodeURIComponent(client_id)}`, {
+			method: "post",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify({
+				name: appDetails.name,
+				description: appDetails.description,
+				icon: appDetails.icon,
+				callbackURLs: appDetails.callbackurls,
+				session: api.session
+			})
+		});
+		$("#saveapp").removeAttribute("disabled");
+		$("#saveapp").textContent = "Save";
+		$("#saveapp").classList.remove("loading", "small");
+	});
+
+	$("#deleteapp").addEventListener("click", async (e) => {
+		$("#deleteconfirm").showModal();
+	});
+
+	$("#canceldelete").addEventListener("click", (e) => {
+		e.preventDefault();
+		$("#deleteconfirm").close();
+		$("#confirmdeleteinput").value = "";
+	});
+
+	$("#confirmdeleteinput").addEventListener("input", (e) => {
+		if (e.target.value === appDetails.name) {
+			$("#confirmdelete").removeAttribute("disabled");
+		} else {
+			$("#confirmdelete").setAttribute("disabled", "true");
+		}
+	});
+
+	$("#confirmdelete").addEventListener("click", async (e) => {
+		e.preventDefault();
+		$("#confirmdelete").setAttribute("disabled", "true");
+		$("#confirmdelete").textContent = "";
+		$("#confirmdelete").classList.add("loading", "small");
+		await fetch(`${api.host}/v4/developer/deleteapp.sjs?client_id=${encodeURIComponent(client_id)}`, {
+			method: "post",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify({
+				session: api.session
+			})
+		});
+		window.location.href = `/developer/`;
+	});
+
+	setLoggedinState(api.loggedIn);
+});
